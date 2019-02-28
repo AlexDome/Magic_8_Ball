d7ada34a9ec30188fb65bf8096d342ba1f027519
Alex Dome <alex.dome.2022@student.lhschools.org>
2019-02-28 13:29:52 -0500

    Added if statement for 1-30

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -3,26 +3,33 @@ import java.util.Scanner;
 public class Magic8Ball {
 
     public static void main(String[]args) {
-        //                             Answers
-        //Yes: Yes | That is very likely | That is Correct! | Indeed | Yep!
-        //No: No. | Probably not. | Very unlikely. | Never. | Not at all.
         //declare
-        int value;
-        int testing;
         Scanner keyboard;
         keyboard = new Scanner(System.in);
 
         //initialize
         String userName;
-        String number;
+        int number;
 
         //code
         System.out.println("Hello, what is your name?");
         userName = keyboard.nextLine();
         System.out.println("Hello " + userName + "! Welcome to the Magic 8 Ball!");
         System.out.println("Please " + userName + " try to guess a number 1-100.");
-        number = keyboard.nextLine();
-
+        number = keyboard.nextInt();
+            if (number<= 10);
+        {
+            System.out.println("Your day will not be enjoyable.");
+        }
+            if (number > 10 && number <= 20);
+        {
+            System.out.println("Your will have a good day.");
+        }
+            if (number > 10 && number > 20 && number <= 30);
+        {
+            System.out.println("Your day will be filled with joy.");
+        }
+        }
     }
-}
+
 
